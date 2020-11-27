65ef2f0a48ad74abf4d7c99152f027f4f3085c08
Eimyatnoeaung <annabel00789@gmail.com>
2020-11-27 20:09:13 +0630

    create our login and register design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,36 @@
 @extends("layout.authlayout")
 @section("content")
-    <h1 class="purple-text">Login Page</h1>
+<div class="container">
+    <div class="col-md-4 offset-4">
+     <!-- Default form login -->
+<form class="text-center border border-light p-5" action="#!">
+
+    <p class="h4 mb-4 pink-text">Sign in</p>
+
+    <!-- Email -->
+    <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+    <!-- Password -->
+    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
+
+    <div class="d-flex justify-content-around">
+
+        <div>
+            <!-- Forgot password -->
+            <a href="">Forgot password?</a>
+        </div>
+    </div>
+
+    <!-- Sign in button -->
+    <button class="btn pink white-text btn-block my-4" type="submit">Sign in</button>
+
+    <!-- Register -->
+    <p>Not a member?
+        <a href="{{ route("register") }}">Register</a>
+    </p>
+
+</form>
+<!-- Default form login -->
+    </div>
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,65 @@
 @extends("layout.authlayout")
 @section("content")
-    <h1 class="yellow-text">Register Page</h1>
+<div class="container">
+    <div class="col-md-4 offset-4">
+        <!-- Material form register -->
+<div class="card mt-5">
+
+    <h5 class="card-header pink white-text text-center py-4">
+        <strong>Register</strong>
+    </h5>
+
+    <!--Card content-->
+    <div class="card-body px-lg-5 pt-0">
+
+        <!-- Form -->
+        <form class="text-center" style="color: #757575;" action="#!">
+
+            <div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <label for="materialRegisterFormFirstName">Username</label>
+                    </div>
+                </div>
+            </div>
+
+            <!-- E-mail -->
+            <div class="md-form mt-0">
+                <input type="email" id="materialRegisterFormEmail" class="form-control">
+                <label for="materialRegisterFormEmail">E-mail</label>
+            </div>
+
+            <!-- Password -->
+            <div class="md-form">
+                <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                <label for="materialRegisterFormPassword">Password</label>
+                <small id="materialRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+                    {{-- At least 8 characters and 1 digit --}}
+                </small>
+            </div>
+            <label for="">Select Your Profile Picture</label>
+            <div class="md-form">
+
+                <input type="file" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+            </div>
+
+            <!-- Sign up button -->
+            <button class="btn btn-outline-pink btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Register</button>
+            <p>
+                <a href="{{ route("login") }}">Already Register?</a>
+            </p>
+
+        </form>
+        <!-- Form -->
+
+    </div>
+
+</div>
+<!-- Material form register -->
+    </div>
+</div>
+
 @endsection
 
