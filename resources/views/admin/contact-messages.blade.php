4f7d681110a7498f642be4952735c6149ff54074
Eimyatnoeaung <annabel00789@gmail.com>
2020-12-09 11:36:37 +0630

    create manage-premium-user table and contact-messages table

--- resources/views/admin/contact-messages.blade.php
+++ resources/views/admin/contact-messages.blade.php
@@ -1,4 +1,27 @@
 @extends('layout.adminlayout')
 @section('content')
 <h1>Contact Message page</h1>
+<table class="table table-hover ">
+    <thead class="green white-text">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Username</th>
+        <th scope="col">Email</th>
+        <th scope="col">Message</th>
+        <th scope="col">Update</th>
+        <th scope="col">Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Anna</td>
+        <td>anna@gmail.com</td>
+        <td>hi admin</td>
+        <td><button class="btn btn-sm green white-text">Update</button></td>
+        <td><button class="btn btn-sm red white-text">Delete</button></td>
+      </tr>
+
+    </tbody>
+  </table>
 @endsection
--- resources/views/admin/manage-premium-user.blade.php
+++ resources/views/admin/manage-premium-user.blade.php
@@ -1,4 +1,29 @@
 @extends('layout.adminlayout')
 @section('content')
 <h1>Manage Premium User page</h1>
+<table class="table table-hover ">
+    <thead class="green white-text">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Username</th>
+        <th scope="col">Email</th>
+        <th scope="col">IsAdmin</th>
+        <th scope="col">IsPremium</th>
+        <th scope="col">Update</th>
+        <th scope="col">Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Anna</td>
+        <td>anna@gmail.com</td>
+        <td>true</td>
+        <td>true</td>
+        <td><button class="btn btn-sm green white-text">Update</button></td>
+        <td><button class="btn btn-sm red white-text">Delete</button></td>
+      </tr>
+
+    </tbody>
+  </table>
 @endsection
