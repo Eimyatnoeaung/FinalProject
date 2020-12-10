39dfd73f142f9042c382eb83077f568c4ebf6c44
Eimyatnoeaung <annabel00789@gmail.com>
2020-12-10 19:11:01 +0630

    save our register datas in database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 // use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -20,15 +23,23 @@ class AuthController extends Controller
              "image"=>"required",
          ]);
          if($validation){
-             //save to database user-tables
+             //move our image to public
+             //  save image name only to database
              $image=request('image');  //move image to image file in public folder path
              $image_name=uniqid()."_".$image->getClientOriginalName(); //save to database
              //uniqid() is for id that laravel give randomly
              //sdg8198_1.jpg//sg111a988_1.jpg
-
-             $image->move(public_path('images/profiles'),$image_name) ; //first parameter is the path that u want to move and the second parameter is used for naming the images that we used
             //public_path is to use to target to public folder
-             //  save image name only to database
+             $image->move(public_path('images/profiles'),$image_name) ; //first parameter is the path that u want to move and the second parameter is used for naming the images that we used
+
+            //save to database user-table
+            $password=$validation['password'];
+            $user=new User();
+            $user->name =$validation['username'];
+            $user->email =$validation['email'];
+            $user->password =Hash::make($password);
+            $user->image=$image_name;
+            $user->save();
 
              return redirect()->route("home");
          }else{
